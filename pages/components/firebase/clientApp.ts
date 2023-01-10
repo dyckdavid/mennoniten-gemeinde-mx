fc5f9e3f8e29575fc7a167b546e63527e6f0e892
David Dyck <dyckd15@icloud.com>
2023-01-10 17:41:42 -0600

    Firebase

---
+++ pages/components/firebase/clientApp.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZTs1SKfx2J8k-RLBXJ7RT8Lp93d0k5B8",
+    authDomain: "mennoniten-gemeinde-797ac.firebaseapp.com",
+    projectId: "mennoniten-gemeinde-797ac",
+    storageBucket: "mennoniten-gemeinde-797ac.appspot.com",
+    messagingSenderId: "231523817643",
+    appId: "1:231523817643:web:a708dde12e10b80ad87fee",
+    measurementId: "G-JRJXL0PQZH"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
