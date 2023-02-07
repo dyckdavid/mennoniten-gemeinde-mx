15419c0390a2fec493c1a6e2fd468fb8e84a740b
David Dyck <dyckd15@icloud.com>
2023-02-06 21:50:20 -0600

    BUG

--- pages/sermons/[id].js
+++ pages/sermons/[id].js
@@ -13,7 +13,7 @@ import Head from 'next/head'
 
 export default function Sermon({ sermon }) {
     const router = useRouter();
-    const { title, name, date, link, audio, speaker } = sermon
+    const { title, date, link, audio, speaker, no } = sermon
 
 
     if (!link) {
