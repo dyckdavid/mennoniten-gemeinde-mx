361e5374e5b4d6b717e3b2a8bfdc99506242b93a
David Dyck <dyckd15@icloud.com>
2023-03-13 20:40:27 -0400

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app'
 import Navbar from './components/Navbar'
 import { Fragment } from 'react';
 import { ContextAuthProvider } from '../context/AuthContext';
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import Head from 'next/head'
+
+
 
 
 
@@ -11,6 +15,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Fragment>
       <ContextAuthProvider>
+      <Head>
+                
+                <meta
+                  name="description"
+                  content="Mennoniten - Gemeinde designed by David Dyck"
+                />
+                <link rel="icon" href="/favicon.ico" />
+              </Head>
       <Navbar />
       <Component {...pageProps} />
       </ContextAuthProvider>
