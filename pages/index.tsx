ebebb39291957b4fb0071cdaa116f55d7fba6f7c
dyckdavid <dyckd27@gmail.com>
2023-03-29 17:33:57 -0600

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home() {
         <meta name="description" content="Mennoniten Gemeinde" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json"></link>
       </Head>
       <Center>
 
