3ad8327407733c5fa69c4f36b9b748aa661a306c
dyckdavid <dyckd27@gmail.com>
2023-04-13 17:30:46 -0600

    signin page updates

--- pages/signin.js
+++ pages/signin.js
@@ -1,99 +1,113 @@
 import {
-    TextInput,
-    PasswordInput,
-    Checkbox,
-    Anchor,
-    Paper,
-    Title,
-    Text,
-    Container,
-    Group,
-    Button,
-  } from '@mantine/core';
+  TextInput,
+  PasswordInput,
+  Paper,
+  Title,
+  Container,
+  Button,
+} from '@mantine/core';
 import { useEffect, useState } from 'react';
 import {
-    getAuth,
-    GoogleAuthProvider,
-    GithubAuthProvider,
-    signInWithPopup,
-    signOut
-} from 'firebase/auth'
-import { auth } from '../firebase/config'
-import { signInWithEmailAndPassword } from 'firebase/auth';
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { useRouter } from 'next/router';
-import Link from 'next/link'
-import Head from 'next/head'
+import Head from 'next/head';
+import { auth } from '../lib/config'
 
-
-  
-
-
-
-
-
-
-  
 function AuthenticationTitle() {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
 
-    
-    const [Credentials, setCredentials] = useState({
-      email: "",
-      password: "",
+  const [isLoading, setIsLoading] = useState(false);
+
+  const auth = getAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.push('/admin');
+      }
     });
 
-    const { push } = useRouter();
-
-    const changeUser = (e) => {
-      setCredentials({
-        ...Credentials,
-        [e.target.name]: e.target.value,
-      });
+    return () => {
+      unsubscribe();
     };
+  }, []);
 
-    const loginUser = async () => {
-      try {
-        await signInWithEmailAndPassword(
-          auth,
-          Credentials.email,
-          Credentials.password
-        );
-        push("/admin");
-      } catch({message}) {
-        if (message === "Firebase: Error (auth/wrong-password).") {
-          console.log("error")
-        }
-      }
-    };
+  const changeUser = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  const loginUser = async () => {
+    setIsLoading(true);
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        credentials.email,
+        credentials.password
+      );
+      router.push('/admin');
+    } catch (error) {
+      console.error('Error signing in:', error);
+      setIsLoading(false);
+    }
+  };
 
+  const isButtonDisabled = !credentials.email || !credentials.password;
 
-
-    return (
-      <><Head>
-
+  return (
+    <>
+      <Head>
         <title>Signin</title>
-      </Head><Container size={420} my={40}>
-          <Title
-            align="center"
-            sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+      </Head>
+      <Container size={420} my={40}>
+        <Title
+          align="center"
+          sx={(theme) => ({
+            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+            fontWeight: 900,
+          })}
+        >
+          Signin
+        </Title>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <TextInput
+            label="email"
+            placeholder="email"
+            onChange={changeUser}
+            name="email"
+            required
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Password"
+            onChange={changeUser}
+            name="password"
+            required
+            mt="md"
+          />
+          <Button
+            fullWidth
+            mt="xl"
+            onClick={loginUser}
+            loading={isLoading}
+            disabled={isButtonDisabled}
           >
-            Signin
-          </Title>
+            Sign In
+          </Button>
+        </Paper>
+      </Container>
+    </>
+  );
+}
 
-
-          <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-            <TextInput label="email" placeholder="email" onChange={changeUser} name="email" required />
-            <PasswordInput label="Password" placeholder="Password" onChange={changeUser} name="password" required mt="md" />
-            <Group position="apart" mt="lg">
-
-            </Group>
-            <Button fullWidth mt="xl" onClick={loginUser}>
-              Sign In
-            </Button>
-
-          </Paper>
-        </Container></>
-    );
-  }
-
-  export default AuthenticationTitle;
+export default AuthenticationTitle;
