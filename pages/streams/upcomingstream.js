b375b0d216a90a8c20758125c02ffef3b40278f0
dyckdavid <dyckd27@gmail.com>
2023-10-26 11:44:45 -0600

    some upcoming strem updates UI

--- pages/streams/upcomingstream.js
+++ pages/streams/upcomingstream.js
@@ -5,139 +5,83 @@ import { Card, Text } from '@mantine/core';
 import { collection, getDocs } from 'firebase/firestore';
 
 export default function UpcomingStream() {
-  // ... (your existing useEffects and variable declarations)
-  
+  const [countdown, setCountdown] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const docSnap = await getDocs(collection(db, 'streamslive'));
+      if (!docSnap.empty) {
+        const dateTimeString = `${docSnap.docs[0].data().date} ${docSnap.docs[0].data().time}`;
+        const endDate = new Date(dateTimeString).getTime();
+        const now = Date.now();
 
-const [countdown, setCountdown] = useState(null);
-
-useEffect(() => {
-  const fetchData = async () => {
-    const docSnap = await getDocs(collection(db, 'streamslive'));
-    
-    if (!docSnap.empty) {
-      const dateTimeString = `${docSnap.docs[0].data().date} ${docSnap.docs[0].data().time}`;
-      const endDate = new Date(dateTimeString).getTime();
-      const now = Date.now();
-
-      if (endDate > now) {
-        const diffTime = Math.abs(endDate - now);
-        setCountdown(diffTime);
+        if (endDate > now) {
+          const diffTime = Math.abs(endDate - now);
+          setCountdown(diffTime);
+        }
       }
-    }
-  };
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
-useEffect(() => {
-  const timerId = setInterval(() => {
-    setCountdown((prevCountdown) => {
-      if (prevCountdown > 1000) {
-        return prevCountdown - 1000;
-      } else {
-        clearInterval(timerId);
-        return 0;
-      }
-    });
-  }, 1000);
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown > 1000) {
+          return prevCountdown - 1000;
+        } else {
+          clearInterval(timerId);
+          return 0;
+        }
+      });
+    }, 1000);
 
-  return () => clearInterval(timerId);
-}, [countdown]);
+    return () => clearInterval(timerId);
+  }, [countdown]);
 
-const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
-const hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
-const minutes = Math.floor((countdown / 1000 / 60) % 60);
-const seconds = Math.floor((countdown / 1000) % 60);
+  const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countdown / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((countdown / 1000 / 60) % 60);
+  const seconds = Math.floor((countdown / 1000) % 60);
 
   return (
     <>
       <Head>
-
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* Any content you previously had in the Head component */}
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <style jsx global>{`
-
-
         .container {
           padding: 0 10px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          
+        }
+
+        .countdownCard {
+          padding: 20px;
+          border-radius: 10px;
+          background-color: #f5f5f5;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
         }
 
         .countdownText {
-          font-size: 20px; 
-          white-space: nowrap;
-        }
-
-        @media (min-width: 601px) {
-          @media (min-width: 601px) {
-            .imageCard {
-              width: 250px;
-              height: 150px;
-              margin: 20px auto;
-            }
-          }
-          
-          
+          font-size: 30px;
+          font-weight: bold;
+          color: red;
         }
       `}</style>
 
       <div className="container">
-        <Card className="imageCard" style={{ 
-          width: '100%', 
-          height: 'auto', 
-          margin: '20px auto', 
-          backgroundColor: '#fff',
-          position: 'relative', 
-          borderRadius: '10px' 
-        }}>
-          <div style={{
-            width: '100%',
-            paddingTop: '66.66%', 
-            position: 'relative',
-            backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/mennoniten-gemeinde-797ac.appspot.com/o/images%2Fkm5church-min3.png?alt=media&token=f7b705ff-e3d7-43f8-a205-82b2bf62aa9f')", 
-            backgroundSize: 'cover', 
-            backgroundPosition: 'center', 
-            backgroundRepeat: 'no-repeat', 
-            opacity: '0.7'
-          }}>
-            <Text 
-              size="xl" 
-              align="center" 
-              style={{ 
-                position: 'absolute',
-                top: '10px',
-                left: '50%',
-                transform: 'translateX(-50%)',
-                fontSize: '30px', 
-                fontWeight: 'bold', 
-                color: 'white' 
-              }}
-            >
-              Gottes Dients
-            </Text>
-
-            <Text 
-              size="xl" 
-              align="center" 
-              className="countdownText"
-              style={{ 
-                position: 'absolute',
-                bottom: '10px',
-                left: '50%',
-                transform: 'translateX(-50%)',
-                fontWeight: 'bold', 
-                color: 'red' 
-              }}
-            >
-              {countdown !== null 
-                ? `Countdown: ${days}d ${hours}h ${minutes}m ${seconds}s` 
-                : 'Loading...'}
-            </Text>
-          </div>
+        <Card className="countdownCard">
+          <Text size="xl" align="center" style={{ fontSize: '40px', fontWeight: 'bold', color: 'black' }}>
+          Nächster Live-Stream
+          </Text>
+          <Text size="xl" align="center" className="countdownText">
+            {countdown !== null ? ` ${days}d ${hours}h ${minutes}m ${seconds}s` : 'Loading...'}
+          </Text>
         </Card>
       </div>
     </>
