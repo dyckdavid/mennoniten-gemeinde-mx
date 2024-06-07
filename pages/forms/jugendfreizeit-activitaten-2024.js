9dc6622027e88f7847cfa2ed45bb669ae34c6e50
David Dyck <dyckd27@gmail.com>
2024-06-06 18:03:28 -0600

    added new form

---
+++ pages/forms/jugendfreizeit-activitaten-2024.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { db } from "../../lib/config";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import styles from '../../styles/MonthCalendar.module.css';
+import Head from 'next/head'
+import { Text, Space, Title, Center } from '@mantine/core';
+import Image from "next/image";
+
+
+
+export default function Jugendfreizeit() {
+    useEffect(() => {
+        // Add the 'adminPage' class to the body when this component mounts
+        document.body.classList.add('adminPage');
+
+        // Remove the 'adminPage' class from the body when this component unmounts
+        return () => {
+            document.body.classList.remove('adminPage');
+        };
+    }, []);
+
+    return(
+        <>
+        <div className={styles.noBackground}>
+            <Head >
+                <title>Jugendfreizeit-Aktivitäten - Mennoniten Gemeinde</title>
+                <meta name="description" content="Mennoniten Gemeinde" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Space h="xl" />
+            <Title fw={700} ta="center">Jugendfreizeit-Aktivitäten</Title>
+            <Space h="xl" />
+            <Center>
+            <iframe className='forms-iframe' src="https://docs.google.com/forms/d/e/1FAIpQLSdjnlxR1GVhSEgixh5Wk3U_V8oqPltjzM4sQEvG2SnZAYS35g/viewform?embedded=true" width="640" height="1441" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </Center>
+            </div>
+        </>
+    );
+}
