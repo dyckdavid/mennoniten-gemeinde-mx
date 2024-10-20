e0905fe3aa9abe68993e9802bc7b0bd3dce5789a
David Dyck <70151610+dyckdavid@users.noreply.github.com>
2024-10-20 16:23:29 -0600

    Update jugendtag2024.js

--- pages/forms/jugendtag2024.js
+++ pages/forms/jugendtag2024.js
@@ -30,12 +30,11 @@ export default function Jugendfreizeit() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Space h="xl" />
-            <Title fw={700} ta="center">JUGENDTAG 2024</Title>
-            <Center><h3>Anmeldeschluss 17 OCT.</h3></Center>
+            <Title fw={700} ta="center">Forms</Title>
             <Space h="xl" />
             <Center>
-            <iframe className='forms-iframe' src="https://docs.google.com/forms/d/e/1FAIpQLSdGisEwbKRJ40mIrtZsTEDJ8dQEnxlXs-aLvXOhDHBL1McPyA/viewform?embedded=true" width="640" height="974" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-            {/* <p>No forms found!</p> */}
+            {/* <iframe className='forms-iframe' src="" width="640" height="974" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
+            <p>No forms found!</p>
             </Center>
             </div>
         </>
